ba0d06c5fb4286912cdcfa6bfbf3b6dec83d6933
Harshil-12 <79490126+Harshil-12@users.noreply.github.com>
2021-12-07 18:51:40 -0500

    All Updated

---
+++ signin.js
@@ -0,0 +1,14 @@
+var objpeople = [ 
+{
+    uname: "psu",
+    psw: "Pennstate"
+}
+]
+
+function login() {
+    var uname = document.getElementById("uname").value
+    var psw = document.getElementById("psw").value
+
+    
+
+}
--- signup.js
+++ signup.js
@@ -1,3 +1,5 @@
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require("constants");
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const signupForm = document.querySelector("#createaccount")
